7fe3d8d1e14d5956424e7e422455ea7eb2784d4b
Varun Gulati <varungulati123@gmail.com>
2024-04-12 13:20:36 +0530

    Configuring environments and adding Google login.

---
+++ app.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+        CLIENT_ID: '952931399118-4meesl46evvv6m4bb7ogfeo4sgbponke.apps.googleusercontent.com',
+        REDIRECT_URI: 'http://localhost:8081'
+    },
+    staging: {
+        CLIENT_ID: '952931399118-4meesl46evvv6m4bb7ogfeo4sgbponke.apps.googleusercontent.com',
+        REDIRECT_URI: 'http://localhost:8081'
+    },
+    production: {
+        CLIENT_ID: '952931399118-4meesl46evvv6m4bb7ogfeo4sgbponke.apps.googleusercontent.com',
+        REDIRECT_URI: 'http://localhost:8081'
+    },
+};
--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,54 +1,66 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleProp, ViewStyle, Alert } from 'react-native';
-import { useAuthRequest } from 'expo-auth-session';
+import {
+    TouchableOpacity,
+    Text,
+    StyleProp,
+    ViewStyle,
+    Alert
+} from 'react-native';
+import {useAuthRequest} from 'expo-auth-session';
+import AppConfig from '../../app.config';
+
+const CLIENT_ID = AppConfig[process.env.NODE_ENV].CLIENT_ID;
+const REDIRECT_URI = AppConfig[process.env.NODE_ENV].REDIRECT_URI;
 
 interface TextStyles {
-  color?: string;
-  fontWeight?: 'bold' | 'normal' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
-  // Add other text styles as needed
+    color?: string;
+    fontWeight?: 'bold' | 'normal' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
+    // Add other text styles as needed
 }
 
 interface LoginButtonProps {
-  styles?: {
-    menuItem?: StyleProp<ViewStyle>;
-    primaryButton?: StyleProp<ViewStyle>;
-    buttonText?: StyleProp<TextStyles>;
-    fullWidthButton?: StyleProp<ViewStyle>;
-  };
+    styles?: {
+        menuItem?: StyleProp<ViewStyle>;
+        primaryButton?: StyleProp<ViewStyle>;
+        buttonText?: StyleProp<TextStyles>;
+        fullWidthButton?: StyleProp<ViewStyle>;
+    };
 }
 
-const LoginButton: React.FC<LoginButtonProps> = ({ styles }) => {
-  const [request, response, promptAsync] = useAuthRequest({
-    clientId: '952931399118-4meesl46evvv6m4bb7ogfeo4sgbponke.apps.googleusercontent.com',
-    scopes: ['openid', 'profile', 'email'],
-    redirectUri: 'http://localhost:8081', // replace with your redirect URL
-  }, {
-    authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
-    tokenEndpoint: 'https://oauth2.googleapis.com/token',
-  });
+const LoginButton: React.FC<LoginButtonProps> = ({styles}) => {
+    const [request, response, promptAsync] = useAuthRequest({
+        clientId: CLIENT_ID,
+        scopes: ['openid', 'profile', 'email'],
+        redirectUri: REDIRECT_URI, // replace with your redirect URL
+    }, {
+        authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+        tokenEndpoint: 'https://oauth2.googleapis.com/token',
+    });
 
-  React.useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params;
-      console.log('Google ID token:', id_token);
-    } else if (response?.type === 'error') {
-      Alert.alert('Authentication error', 'An error occurred while logging in with Google.');
-    }
-  }, [response]);
+    React.useEffect(() => {
+        if (response?.type === 'success') {
+            const {id_token} = response.params;
+            console.log('Google ID token:', id_token);
+        } else if (response?.type === 'error') {
+            Alert.alert('Authentication error', 'An error occurred while logging in with Google.');
+        }
+    }, [response]);
 
-  const handleLogin = async () => {
-    if (request) {
-      await promptAsync();
-    } else {
-      Alert.alert('Authentication error', 'Authentication request could not be created.');
-    }
-  };
+    const handleLogin = async () => {
+        if (request) {
+            await promptAsync();
+        } else {
+            Alert.alert('Authentication error', 'Authentication request could not be created.');
+        }
+    };
 
-  return (
-    <TouchableOpacity style={[styles?.menuItem, styles?.primaryButton, styles?.fullWidthButton]} onPress={handleLogin}>
-      <Text style={styles?.buttonText}>Login with Google</Text>
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity
+            style={[styles?.menuItem, styles?.primaryButton, styles?.fullWidthButton]}
+            onPress={handleLogin}>
+            <Text style={styles?.buttonText}>Login with Google</Text>
+        </TouchableOpacity>
+    );
 };
 
 export default LoginButton;
